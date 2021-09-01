e5a75d8b7ba4df1b4ad434f3236ae4b1e25f0293
andreforjaz <49514576+andreforjaz@users.noreply.github.com>
2021-09-01 10:11:28 -0400

    images paths v2

--- TMA_auto_registration_AP10.py
+++ TMA_auto_registration_AP10.py
@@ -1,15 +1,14 @@
-
-# Images Path
+## Paths
 
 pth = '\\babyserverdw4\Pei-Hsun Wu\digital pathology image data\IF HE co-stain AM\TMA analysis\TMA2';
 pthdata = '\\babyserverdw4\Pei-Hsun Wu\digital pathology image data\IF HE co-stain AM\TMA analysis\TMA2\data TMA2';
 
-% Load path of image folders
+# Load path of image folders
 pthhe = '\\babyserverdw4\Pei-Hsun Wu\digital pathology image data\IF HE co-stain AM\TMA section 2 IF run1 AM\H&E image';
 pthif = '\\babyserverdw4\Pei-Hsun Wu\data from SFC imager\02142020 PW IHC Breast Tumor TMA Imaging take 2 dapi CD8 foxp3 Cd31 panck\well image stitch';
 
-% Path Saved images
-% 20x IF
+# Path Saved images
+# 20x IF
 
 pthdapi = '\\babyserverdw4\Pei-Hsun Wu\data from SFC imager\02142020 PW IHC Breast Tumor TMA Imaging take 2 dapi CD8 foxp3 Cd31 panck\well image stitch\1x_\htcpID309_well01_TMA20200213PW_stitch1_c1_1x_.tif';
 pthcd8 = '\\babyserverdw4\Pei-Hsun Wu\data from SFC imager\02142020 PW IHC Breast Tumor TMA Imaging take 2 dapi CD8 foxp3 Cd31 panck\well image stitch\1x_\htcpID309_well01_TMA20200213PW_stitch1_c2_1x_.tif';
